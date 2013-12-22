adfc5666f4b6762113e808452f4ec808603513ca
Prazzy <prazzee@gmail.com>
2013-12-22 14:07:41 -0800

    Reverse Linked List function

--- linked_list_javascript.js
+++ linked_list_javascript.js
@@ -1,10 +1,9 @@
-
-function MakeNode(size) {
+function MakeList(size) {
    head = {'data':'', 'next':null};
    prev = head;
    for (var i=0;i<size;i++){
        var curr_obj = {};
-       curr_obj['data'] = i+1;
+       curr_obj.data = i+1;
        prev.next = curr_obj;
        prev = curr_obj;
    }
@@ -19,3 +18,25 @@ function PrintNodes(head) {
     node = node.next;  
   }
 }
+
+console.log(PrintNodes(MakeList(3)));
+
+function ReverseList(head) {
+   var node = head.next;
+    head.next = {};
+   var prev = null;
+   while (node) {
+       if (node.next) {saved = node.next;}
+       else {head.next = node; saved=null;}
+     node.next = prev;
+     prev = node;
+     node = saved;
+   }
+  return head;
+}
+
+console.log(PrintNodes(ReverseList(MakeList(3))));
+
+
+
+
